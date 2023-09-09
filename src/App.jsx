2ee95a54a0137444866c792a684019fe7cbcf90e
DannyRPR <danper527@gmail.com>
2023-09-08 20:54:18 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ function App() {
       <p> Lorem ipsum dolor, sit amet consectetur ad commodi temporibus, eius illum porro dicta ipsum in. Ipsum alias nobis autem sunt assumenda accusantium corrupti vel eveniet quae consequuntur maiores consectetur, dolor, repellendus laborum impedit, et labore! Beatae illo eveniet necessitatibus ea. Dicta expedita nostrum vero nobis soluta modi cum eos cumque maiores maxime, assumenda doloribus, tempora tempore porro harum doloremque, quisquam quia nam. Omnis quia officiis consectetur a vero vitae repellendus asperiores debitis qui sed, dolor, cum enim, magnam nemo ipsum deserunt quidem obcaecati culpa ipsa sapiente velit provident sequi nulla quo. Veniam, error numquam explicabo tempore esse delectus amet ullam voluptas neque perferendis quam laborum quae expedita at dolore. Magnam provident eius, blanditiis esse ducimus ab iste illo tempore, at est corrupti. Ipsam in temporibus libero aspernatur tempora delectus dignissimos exercitationem sequi culpa dolorem voluptate animi iure neque consectetur voluptatibus nesciunt dolor ut maxime alias quas ratione, dolores ad sint? Assumenda nostrum maiores natus porro sunt possimus? Assumenda vitae doloribus aliquid odio fugiat, dignissimos qui soluta maxime corporis! </p>
     </div>
    */}
+   <h3>STORE</h3>
   <Card/>
+  <div className="contacto">
+  <h2>Ing. Danny Portilla R</h2>
+  <span><b>Contacto:</b> 3185679541</span>
+  </div>
+  
     </>
   );
 }
